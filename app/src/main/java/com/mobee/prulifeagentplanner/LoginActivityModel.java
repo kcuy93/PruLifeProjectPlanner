0aa6a392bf3c018bf1d48ea5565945833e11a826
ristelfrancis <castro.ristel@gmail.com>
2016-08-11 23:22:38 +0800

    Check file diff

--- app/src/main/java/com/mobee/prulifeagentplanner/LoginActivityModel.java
+++ app/src/main/java/com/mobee/prulifeagentplanner/LoginActivityModel.java
@@ -1,118 +1,77 @@
 package com.mobee.prulifeagentplanner;
 
-import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class LoginActivityModel extends AppCompatActivity {
 
     private static final String TAG = "LoginActivityModel";
-    boolean loggedIn = false;
+    boolean auth = false;
+    boolean signIn = false;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
 
     public boolean login(final String emailAddress, final String password) {
+        boolean loggedIn = false;
         mAuth = FirebaseAuth.getInstance();
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     Log.d(TAG, "onAuthStateChanged:USER: " + user.getUid() + "signed in.");
-                    loggedIn = true;
+                    auth = true;
                 } else {
                     Log.d(TAG, "onAuthStateChanged:User logged out.");
+                    auth = false;
                 }
-                signIn(emailAddress, password);
             }
         };
+
+        mAuth.signInWithEmailAndPassword(emailAddress, password).addOnCompleteListener(LoginActivityModel.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInWithEmailAndPassword");
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signIn:onComplete failed", task.getException());
+                    Toast.makeText(LoginActivityModel.this, R.string.authFailed, Toast.LENGTH_SHORT).show();
+                    signIn = false;
+                } else {
+                    Log.d(TAG, "signIn:onComplete successful: " + task.isSuccessful());
+                    signIn = true;
+                }
+            }
+        });
+
+        if (auth || signIn) {
+            loggedIn = true;
+        }
         return loggedIn;
     }
 
+
     public void onStart() {
         super.onStart();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
         mAuth.addAuthStateListener(mAuthListener);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "LoginActivityModel Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.mobee.prulifeagentplanner/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+
     }
 
     public void onStop() {
         super.onStop();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "LoginActivityModel Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.mobee.prulifeagentplanner/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
         if (mAuth != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.disconnect();
     }
-
-    private void signIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.d(TAG, "signIn:onComplete: " +  task.isSuccessful());
-                if(!task.isSuccessful()){
-                    Log.w(TAG, "signIn:onComplete failed", task.getException());
-                    Toast.makeText(LoginActivityModel.this, R.string.authFailed, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-
 
 }
--- app/src/main/java/com/mobee/prulifeagentplanner/LoginActivityPresenter.java
+++ app/src/main/java/com/mobee/prulifeagentplanner/LoginActivityPresenter.java
@@ -1,15 +1,14 @@
 package com.mobee.prulifeagentplanner;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.mobee.prulifeagentplanner.LoginActivityModel;
 
 public class LoginActivityPresenter extends AppCompatActivity {
 
@@ -31,9 +30,49 @@ public class LoginActivityPresenter extends AppCompatActivity {
         login = (Button) findViewById(R.id.loginButton);
         register = (TextView) findViewById(R.id.requestAccess);
 
+        //password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+
+       email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email.setText("");
+            }
+        });
+
+
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.setText("");
+            }
+        });
+
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                email.setHint(R.string.agentCode);
+            }
+        });
+
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                password.setHint("");
+            }
+        });
+
+
         emailAddress = email.getText().toString();
         passWord = password.getText().toString();
 
+
+        /**
+         * Upon clicking login button, email & password will be validated
+         * when both email and password fields are validated
+         * it will call login function in LoginActivityModel and pass email & password
+         * if login function returns true, login is successful
+         * **/
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -61,12 +100,12 @@ public class LoginActivityPresenter extends AppCompatActivity {
 
     private boolean validateEmail()
     {
-        boolean valid = false;
+        boolean valid = true;
         String emailAddress = email.getText().toString();
         if(TextUtils.isEmpty(emailAddress))
         {
             email.setError("Email required");
-            valid = true;
+            valid = false;
         }
 
         return valid;
@@ -74,11 +113,12 @@ public class LoginActivityPresenter extends AppCompatActivity {
 
     private boolean validatePassword()
     {
-        boolean valid = false;
+        boolean valid = true;
         String pass = password.getText().toString();
         if(TextUtils.isEmpty(pass))
         {
             password.setError("Password required");
+            valid = false;
         }
         return valid;
     }
